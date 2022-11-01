1903f96ffca7a6cd32d49bbaa91b0e6cb3cb720a
Gloria Ngoya <glowngoya@gmail.com>
2022-11-01 18:40:46 +0300

    Gallery Commit

--- app/src/main/java/com/gloria/glowapp/MainActivity.java
+++ app/src/main/java/com/gloria/glowapp/MainActivity.java
@@ -9,17 +9,26 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private Button select, predict;
+    private TextView tv;
+    private Bitmap img;
+
+
+
 //Initialize Variable
     ImageView imageView;
     Button btOpen;
-    Button btnStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,10 +38,22 @@ public class MainActivity extends AppCompatActivity {
         //Assign Value
         imageView = findViewById(R.id.image_view);
         btOpen = findViewById(R.id.button_camera);
-        btnStorage = findViewById(R.id.button_gallery);
 
-        Intent myIntent = new Intent(this,SecondActivity.class);
-        startActivity(myIntent);
+        //Initialization
+        tv = (TextView) findViewById(R.id.textView2);
+        predict = (Button) findViewById(R.id.button_predict);
+        select = (Button) findViewById(R.id.button_gallery);
+
+        //The gallery Button
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Opening a new screen to get an image in the devise
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 101);
+            }
+        });
 
         //Request for Camera
         if (ContextCompat.checkSelfPermission(MainActivity.this,
@@ -57,13 +78,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 100){
+        if (requestCode == 100) {
             //Get Capture Image
             Bitmap captureImage = (Bitmap) data.getExtras().get("data");
             //Set Capture Image to ImageView
             imageView.setImageBitmap(captureImage);
 
+            //Get the selected Image and set it to the ImageView
+        }else if (requestCode == 101){
+                imageView.setImageURI(data.getData());
+                Uri uri = data.getData();
+            try {
+                img = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
+            // Intent myIntent = new Intent(this,SecondActivity.class);
+            //        startActivity(myIntent);
+
+        }
     }
-}
+
+
+
